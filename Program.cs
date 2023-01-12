56f6da03e21192957c70a2c9c9a9b01efac2f5de
Alina <lina.millou@yandex.ru>
2023-01-12 13:46:55 +0300

    Итоговая работа

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
 ﻿// Итоговая работа.
+Console.WriteLine("Введите количество элементов массива: ");
+int size  = int.Parse(Console.ReadLine());
+
+string[] arr1 = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	arr1[i] = result;  
+}
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(",", arr1));
+Console.Write("]");
+
+int count = 0;
+int maxSymbols = 3;
+
+for (int i = 0; i < arr1.Length; i++){
+	if(arr1[i].Length <= maxSymbols){
+		count++;
+	}   
+}
+Console.WriteLine();
+Console.Write("Полученный массив: [");
+string[] arr2 = new string[count];
+int j = 0;
+for (int i = 0; i < arr1.Length; i++){
+    if(arr1[i].Length <= maxSymbols){
+        arr2[j] = arr1[i];
+        Console.Write(arr2[j] + ",");
+        j++;
+    }
+}
+Console.Write("]");
